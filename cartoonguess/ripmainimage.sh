68846e34513829d3f73c8cdf219f97cb0bbbaaeb
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-03-06 10:56:13 -0800

    fixed rip use bash script

--- cartoonguess/ripmainimage.sh
+++ cartoonguess/ripmainimage.sh
@@ -1,12 +1,10 @@
 rootURL="http://www.cnntees.com/posts/cartoon-game/"
-thumb="thumbs/"
 mainimage="mainImages/"
+thumb="thumbs/"
 ext=".png"
 
 for i in `seq 1 56`;
 do
-	fullpath=$rootURL$thumb$i$ext 
-	curl -o $thumb$i$ext $fullpath
-	fullpath=$rootURL$mainimage$i$ext 
+	fullpath=$rootURL$thumb$i$ext
 	curl -o $mainimage$i$ext $fullpath
 done
